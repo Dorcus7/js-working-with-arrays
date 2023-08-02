28a6708f383d3593752ea8e0ae246478786f1991
Laura Hackney <laura@anniecannons.com>
2023-08-02 15:15:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Working with Arrays
+
+- An array is a special data type that allows you to store and organize multiple values, such as numbers, strings, or other objects, in a single variable. Arrays are used to create lists, collections, and data structures that can be easily added to and iterated over.
+
+- An array in JavaScript is an ordered collection of elements, where each element has a numeric index (starting from 0) that represents its position in the array. The first element is at index 0, the second element is at index 1, and so on. Arrays can contain elements of different data types, and they can dynamically change in size.
+
+### Array Methods
+
+- In JavaScript, array methods are built-in functions that can be called on arrays to perform various operations on their elements. These methods allow you to change, search, iterate, and perform other actions on the elements of an array conveniently and efficiently.
+
+
+### Prep
+1. Connect your js file to your HTML file
+2. Open your code in the browser and open your console
+
+
+### Exercises
+1. In the `js` file, follow the prompts to complete the exercises.
---
+++ scripts.js
@@ -0,0 +1,115 @@
+/********************** Exercise 1  **********************/
+
+// Go to a website of your choosing (a news site or a shopping site). Find examples of data that might be stored as arrays. Provide a description of the data below. 
+
+
+
+
+
+/********************** Exercise 2  **********************/
+// Create an array of books to be sold in an online store. 
+
+
+
+// Log the array to the console.
+
+
+
+/********************** Exercise 3  **********************/
+const shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+
+// For the above array, log the string "bread" to the console.
+
+
+// After the following code is run, what will happen? Console log the new array.
+shopping[0] = "rice";
+
+
+
+
+/********************** Exercise 4  **********************/
+
+const colors = ["red", "green", "blue", "yellow"];
+// Use the splice method to return the following array. ["red", "green", "yellow"]
+
+
+
+
+
+/********************** Exercise 5  **********************/
+
+const numbers = [5, 1, 8, 3, 10, 2, 7, 4, 6, 9];
+
+// Sort the array in ascending order
+
+
+
+// Print the sorted array
+
+
+
+
+/********************** Exercise 6  **********************/
+// The following array contains data about a user. How would you log the string 'Python' to the console?
+const userMember = ["Dava Sorbel", 795, ['Python', 'JavaScript', 'Ruby']];
+
+
+
+
+
+
+/********************** Exercise 7  **********************/
+
+const cities = ["Oakland", "New York"];
+
+
+// How would you dynamically add Memphis to the array of cities?
+
+
+//How would you dynamically add Atlanta and Denver to the array of cities at the same time?
+
+
+// How would you remove Oakland from the array? 
+
+
+// Log the final array to the console. 
+
+
+/********************** Exercise 8  **********************/
+
+const cityString = "Detroit,Atlanta,Birmingham,New Haven,Dallas,San Diego,Portland";
+
+// Now let's split the array at each comma. How would you create an array with each of the cities as a separate value?
+
+
+// What is the length of the new array?
+
+
+// Log the array to the console
+
+
+// How would you log the last city in the array to the console?
+
+
+// Using the .join() method, how would you turn the list of cities back into a string with spaces after the commas?
+
+
+/********************** Exercise 9  **********************/
+
+
+let myString = 'Ryu+Ken+Chun-Li+Cammy+Guile+Sakura+Sagat+Juri';
+
+// Convert the string into an array, removing the + characters in the process. Save the result in a variable called myArray.
+
+
+// Store the length of the array in a variable called arrayLength.
+
+
+// Store the last item in the array in a variable called lastItem.
+
+
+// Remove the last item in the array.
+
+
+
+// Add two new names to the end of the array.
